a6d3d4dc74d7b1b548952f39bb49fdf9479c5c49
Hemant Kaushik <hemantka69@gmail.com>
2025-12-23 10:44:43 +0530

    [Hemant Kaushik] Add. Solutions of EXTRAS Java String Practice Problems

---
+++ core-java-practice/gcr-codebase/string/extraString_Problems/RemoveCharacterFromString.java
@@ -0,0 +1,30 @@
+package extraString_Problems;
+import java.util.*;
+public class RemoveCharacterFromString {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        System.out.print("Enter the character to remove: ");
+        char toRemove = scanner.next().charAt(0);
+
+        String result = removeCharacter(input, toRemove);
+
+        System.out.println("Modified String: " + result);
+    }
+
+    public static String removeCharacter(String str, char ch) {
+        StringBuilder result = new StringBuilder();
+
+        for (char current : str.toCharArray()) {
+            if (current != ch) {
+                result.append(current);
+            }
+        }
+
+        return result.toString();
+    }
+}
---
+++ core-java-practice/gcr-codebase/string/extraString_Problems/RemoveDuplicateFromString.java
@@ -0,0 +1,26 @@
+package extraString_Problems;
+import java.util.*;
+public class RemoveDuplicateFromString {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String result = removeDuplicates(input);
+
+        System.out.println("String after removing duplicates: " + result);
+    }
+
+    public static String removeDuplicates(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (result.indexOf(ch) == -1) {
+                result += ch;
+            }
+        }
+        return result;
+    }
+}
